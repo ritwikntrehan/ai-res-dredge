90f93012af6a94ddbcde2b49d7728b9d496bebd0
ritwikntrehan <ritwik.trehan@gmail.com>
2025-07-25 18:58:19 -0700

    Create gen_chain.py

---
+++ chains/gen_chain.py
@@ -0,0 +1,10 @@
+# chains/gen_chain.py
+from langchain import PromptTemplate, LLMChain
+from langchain.llms import OpenAI
+
+gen_prompt = PromptTemplate.from_file("prompts/gen_prompt.tpl")
+gen_llm   = OpenAI(temperature=0.2)
+gen_chain = LLMChain(llm=gen_llm, prompt=gen_prompt)
+
+def generate_resume(facts_json: list, job_posting: str):
+    return gen_chain.run(facts_json=json.dumps(facts_json), job_posting=job_posting)
