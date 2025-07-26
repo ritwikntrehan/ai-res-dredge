70d57cbb9b0aa33a022f0813ea68955ed550a6dc
ritwikntrehan <ritwik.trehan@gmail.com>
2025-07-25 19:24:53 -0700

    Update gen_chain.py

--- chains/gen_chain.py
+++ chains/gen_chain.py
@@ -1,10 +1,11 @@
 # chains/gen_chain.py
-import os
-from langchain import PromptTemplate, LLMChain
+import streamlit as st
+from langchain_core.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from langchain.llms import OpenAI
-
+key = st.secrets["OPENAI_API_KEY"]
 gen_prompt = PromptTemplate.from_file("prompts/gen_prompt.tpl")
-gen_llm   = OpenAI(temperature=0.2, openai_api_key=os.getenv("OPENAI_API_KEY"))
+gen_llm   = OpenAI(temperature=0.2, openai_api_key=key)
 gen_chain = LLMChain(llm=gen_llm, prompt=gen_prompt)
 
 def generate_resume(facts_json: list, job_posting: str):
